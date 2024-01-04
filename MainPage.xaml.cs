9a1084f0c6c7430bd2bc181216879e180ad759c3
Hunter Keating <hkeating27@gmail.com>
2024-01-04 03:14:47 -0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,199 @@
+﻿using Communications;
+using FileLogger;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Internals;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Channels;
+
+namespace ChatClient {
+
+	/// <summary>
+	/// This class represents the logic behind the GUI of a client
+	/// Written By: Nathaniel Taylor
+	/// Debugged By: Hunter Keating and Nathaniel Taylor
+	/// </summary>
+	public partial class MainPage : ContentPage
+	{
+		//Field
+		private Networking network;
+		private string serverName;
+		private string text;
+		private ILogger<MainPage> logger;
+		private List<string> participants;
+		private string clientName;
+
+		/// <summary>
+		/// Initializes the GUI, creates a new Networking object, and initializes the
+		/// serverName and text fields.
+		/// </summary>
+		public MainPage(ILogger<MainPage> logger)
+		{
+			InitializeComponent();
+			network = new Networking(logger, connectionComplete, connectionDropped, messageArrived, '\n');
+			serverAdd.Text = "localhost";
+			serverName = Dns.GetHostName();
+			text = "";
+			clientName = "";
+			this.logger = logger;
+			participants = new List<string>();
+		}
+
+		/// <summary>
+		/// Connects the client to the specified server
+		/// </summary>
+		/// <param name="sender">The sender of the event that triggers this method</param>
+		/// <param name="e">The Event Arguments of the event that triggers this method</param>
+		private void ConnectToServer(object sender, EventArgs e)
+		{
+			//Initializes connection with GUI interaction
+			Label beginConnection = new Label();
+			beginConnection.Text = "Connecting...";
+			sentMessages.Add(beginConnection);
+			network.Connect(serverName, 11000);
+
+			logger.LogInformation("A connection has successfully begun.");
+        }
+
+		/// <summary>
+		/// The callback method that the Networking object calls whenever this client connects to the server
+		/// </summary>
+		/// <param name="client">The client that connected to the server</param>
+		private void connectionComplete(Networking client)
+		{
+			//Lets the user know the connection was successful
+            Label connected = new Label();
+			connected.Text = "Connection successful.";
+            sentMessages.Add(connected);
+
+			//Sets the client ID if given
+			if (clientName != "")
+				client.ID = clientName;
+
+			//New thread between client and server
+            Thread messageThread = new Thread(() => client.AwaitMessagesAsync(infinite: true));
+            messageThread.Start();
+
+			//Logs success
+			logger.LogInformation("A connection has been successfully established.");
+        }
+
+		/// <summary>
+		/// The callback method that the Networking object calls whenever the specified client
+		/// gets disconnected from the server
+		/// </summary>
+		/// <param name="client">The specified client</param>
+		private void connectionDropped(Networking client)
+		{
+			//Lets user know they have been disconnected
+			Label disconnectedLabel = new Label();
+			disconnectedLabel.Text = "You have been disconnected from the server.";
+            Application.Current.Dispatcher.Dispatch((Action)(() => sentMessages.Add(disconnectedLabel)));
+
+			logger.LogInformation("A connection has dropped. This could be because the server was shutdown" +
+								  " or an error has occured.");
+		}
+
+		/// <summary>
+		/// The callback method that the Networking object calls whenever the specified client gets
+		/// sent a message.
+		/// </summary>
+		/// <param name="client">The specified client</param>
+		/// <param name="text">The sent message</param>
+		private void messageArrived(Networking client, string text)
+		{
+			network = client;
+			//GUI interface showing the message
+			Label messageLabel = new Label();
+			messageLabel.Text = text;
+
+			//Gets the name of every participant
+			if (text.Contains("Command Participants,"))
+			{
+				string nameChange = "";
+				for (int i = 0; i <= text.Length; i++)
+				{
+					if (text[i] == '[' && text[i + 1] != ']')
+						nameChange += text[i + 1];
+					else
+						nameChange += text[i];
+
+					if (text[i] == ']')
+					{
+						participants.Add(nameChange);
+					}
+				}
+			}
+			
+			else if (text.StartsWith("This client is in the server:"))
+			{
+				participants.Add(text.Substring(28));
+			}
+			//Otherwise send a normal message
+			else
+			{
+				messageLabel.Text = client.ID + messageLabel.Text;
+				Application.Current.Dispatcher.Dispatch((Action)(() => sentMessages.Add(messageLabel)));
+
+				logger.LogInformation("A message has successfully arrived.");
+			}
+		}
+
+        /// <summary>
+        /// The method called whenever the user changes the name of the server they want to connect to
+        /// </summary>
+        /// <param name="sender">The sender of the event that triggers this method</param>
+        /// <param name="e">The Event Arguments of the event that triggers this method</param>
+        private void ServerAddressChanged(object sender, EventArgs e)
+		{
+			//Changes username
+			serverName = (sender as Entry).Text;
+			//Logs success
+			logger.LogInformation("The server name has successfully been changed.");
+		}
+
+        /// <summary>
+        /// The method called whenever the user changes the message they want to send
+        /// </summary>
+        /// <param name="sender">The sender of the event that triggers this method</param>
+        /// <param name="e">The Event Arguments of the event that triggers this method</param>
+        private void MessageCompleted(object sender, EventArgs e)
+		{
+			//Sets the text and sends it
+			text = (sender as Entry).Text;
+			network.Send(text);
+			//Logs success
+			logger.LogInformation("A message has successfully been written.");
+		}
+
+		/// <summary>
+		/// Changes the ID property of the client's Networking object
+		/// </summary>
+		/// <param name="sender">The entry sending the event that triggers this method</param>
+		/// <param name="e">The Event Arguments of the event that triggers this method</param>
+		private void ChangeID(object sender, EventArgs e)
+		{
+			//Changes the client ID
+			clientName = (sender as Entry).Text;
+			//Logs success
+			logger.LogInformation("The name of the client has successfully been changed.");
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="sender">The button sending the event that triggers this method</param>
+		/// <param name="e">The Event Arguments of the event that triggers this method</param>
+		private void retrieveClients(object sender, EventArgs e)
+		{
+			//Retrieves list of all clients
+			foreach (string name in participants)
+			{
+				Label participantLabel = new Label();
+				participantLabel.Text = name;
+				listOfParticipants.Add(participantLabel);
+			}
+		}
+	}
+}
+
---
+++ Networking.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Communications
+{
+    /// <summary>
+    /// This class represents an object used by the client and server GUIs to connect a client to a server,
+    /// create a server that listens for connections and accepts them, allow clients to wait for messages, allow
+    /// clients to send messages, allow a client to be disconnected, and allow a server to stop allowing clients
+    /// to connect.
+    /// Written By: Nathaniel Taylor and Hunter Keating
+    /// Debugged By: Hunter Keating and Nathaniel Taylor
+    /// </summary>
+    public class Networking
+    {
+        //Delegates
+        public delegate void ReportMessageArrived(Networking channel, string message);
+        public delegate void ReportDisconnect(Networking channel);
+        public delegate void ReportConnectionEstablished(Networking channel);
+
+        //Fields
+        private ReportMessageArrived onMessage;
+        private ReportDisconnect reportDisconnect;
+        private ReportConnectionEstablished onConnect;
+        private CancellationTokenSource cancelSource;
+        private char terminationChar;
+        private TcpClient client;
+        private ILogger logger;
+        private byte[] buffer;
+
+        /// <summary>
+        /// Gets or sets the name of the client
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Creates and sets up a new Networking object
+        /// </summary>
+        /// <param name="logger">a logger object provided via dependency injection</param>
+        /// <param name="onConnect">A callback method to be called when a client successfully connects</param>
+        /// <param name="reportDisconnect">A callback method to be called when a client is disconnected</param>
+        /// <param name="onMessage">A callback method to be called when a client recieves a message</param>
+        /// <param name="terminationCharacter">The character that tells the program when to terminate an action</param>
+        public Networking(ILogger logger, ReportConnectionEstablished onConnect, ReportDisconnect reportDisconnect,
+                          ReportMessageArrived onMessage, char terminationCharacter)
+        {
+            ID                    = "";
+            cancelSource           = new CancellationTokenSource();
+            terminationChar       = terminationCharacter;
+            this.logger           = logger;
+            this.onMessage        = onMessage;
+            this.onConnect        = onConnect;
+            this.reportDisconnect = reportDisconnect;
+            client                = new TcpClient();
+            buffer = new byte[1];
+        }
+
+        /// <summary>
+        /// Creates and sets up a new Networking object with an already connected client
+        /// </summary>
+        /// <param name="logger">a logger object provided via dependency injection</param>
+        /// <param name="client">A previously connected client</param>
+        /// <param name="onConnect">A callback method to be called when a client successfully connects</param>
+        /// <param name="reportDisconnect">A callback method to be called when a client is disconnected</param>
+        /// <param name="onMessage">A callback method to be called when a client recieves a message</param>
+        /// <param name="terminationCharacter">The character that tells the program when to terminate an action</param>
+        private Networking(ILogger logger, TcpClient client, ReportConnectionEstablished onConnect, ReportDisconnect
+                           reportDisconnect, ReportMessageArrived onMessage, char terminationCharacter)
+        {
+            ID = "";
+            cancelSource = new CancellationTokenSource();
+            terminationChar = terminationCharacter;
+            this.logger = logger;
+            this.onMessage = onMessage;
+            this.onConnect = onConnect;
+            this.reportDisconnect = reportDisconnect;
+            this.client = client;
+            buffer = new byte[1];
+        }
+
+        /// <summary>
+        /// Connects a client network to the server network
+        /// </summary>
+        /// <param name="host">the host name of the server</param>
+        /// <param name="port">the port the host is connected to</param>
+        /// <exception cref="Exception">Used only for debugging and logging</exception>
+        public void Connect(string host, int port)
+        {
+            try
+            {
+                client = new TcpClient(host, port);
+                EndPoint? remoteEndPoint = client.GetStream().Socket.RemoteEndPoint;
+                if (remoteEndPoint != null && ID == "")
+                    ID = remoteEndPoint.ToString();
+                onConnect(this);
+            }
+            catch (Exception e)
+            {
+                string message = e.Message;
+                logger.LogInformation(message);
+            }
+        }
+
+        /// <summary>
+        /// A method used by clients to wait for messages from other clients
+        /// </summary>
+        /// <param name="infinite">tells the client whether or not to continuously wait for messages</param>
+        public async void AwaitMessagesAsync(bool infinite = true)
+        {
+            try
+            {
+                //Creates a stream between server and client
+                NetworkStream clientStream = client.GetStream();
+                if (infinite)
+                {
+                    while (infinite)
+                    {
+                        //Checks message length and throws if it is empty
+                        int total = await clientStream.ReadAsync(buffer);
+                        if (total == 0)
+                            throw new Exception("You have been disconnected.");
+                        onMessage(this, System.Text.Encoding.Default.GetString(buffer));
+                        //Clears the buffer, reducing the amount of repeat messages
+                        Array.Clear(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+            catch
+            {
+                //Ends if the user disconnects
+                reportDisconnect(this);
+            }
+        }
+
+        /// <summary>
+        /// A method used by servers to wait for client networks to connect to them
+        /// </summary>
+        /// <param name="port">The port the server is connected and listening to</param>
+        /// <param name="infinte">Tells the client whether or not to continuously wait for clients</param>
+        public async void WaitForClients(int port, bool infinte)
+        {
+            //Listener that takes in any TCP client
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            try
+            {
+                while (infinte)
+                {
+                    //Creates clients and connection to them, as well as establishing their ID
+                    TcpClient connection = await listener.AcceptTcpClientAsync(cancelSource.Token);
+                    Networking newConnection = new Networking(logger, connection, onConnect, reportDisconnect,
+                                                              onMessage, terminationChar);
+                    newConnection.ID = connection.GetStream().Socket.RemoteEndPoint.ToString();
+                    //Methods for connecting
+                    newConnection.AwaitMessagesAsync();
+                    onConnect(newConnection);
+                }
+            }
+            catch
+            {
+                //Stop listening in case infinite is false
+                listener.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Tells the server to stop allowing clients to connect
+        /// </summary>
+        public void StopWaitingForClients()
+        {
+            cancelSource.Cancel();
+        }
+
+        /// <summary>
+        /// Allows clients to disconnect from the server
+        /// </summary>
+        public void Disconnect()
+        {
+            reportDisconnect(this);
+            client.Close();
+        }
+
+        /// <summary>
+        /// Sends a message to a client
+        /// </summary>
+        /// <param name="text">The message to be sent</param>
+        public async void Send(string text)
+        {
+            //Sends message
+            if (!text.Contains(terminationChar))
+            {
+                text += terminationChar;
+                //Changes string to a buffer that the server can read
+                buffer = Encoding.UTF8.GetBytes(text);
+                NetworkStream clientStream = client.GetStream();
+                await clientStream.WriteAsync(buffer);
+                //Logger stores success
+                logger.LogInformation("A message has successfully been sent.");
+            }
+        }
+    }
+}
